0e3468464b7993227f516ed5cb950ac96c7f3e66
Mark Andrews <andrewsmarkdeveloper@gmail.com>
2020-05-11 18:08:31 +0400

    routes change/pass,email,description,cover

--- routes/change_user_data.js
+++ routes/change_user_data.js
@@ -5,7 +5,6 @@ var route = require('express').Router();
 route.post('/change_pass', require('../controllers/change_user_data').change_pass);
 route.post('/change_email', require('../controllers/change_user_data').change_email);
 route.post('/change_description', require('../controllers/change_user_data').change_description);
-
 route.post('/change_cover', require('../controllers/change_user_data').change_cover);
 route.post('/upload_avatar', require('../controllers/change_user_data').upload_avatar);
 
