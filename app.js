b6fe01983ab2d853ca79ab1d9a0b8d761171f779
Mark Andrews <andrewsmarkdeveloper@gmail.com>
2020-05-14 15:34:53 +0400

    Restored right port number

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 const server = require('http').createServer(app);
 const cors = require('cors');
 
